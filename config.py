d8c21d720286a2b7adcd810d3b9f2ba735b79edf
Junyan Su <junyansu@JunyandeMacBook-Air.local>
2024-04-08 09:11:24 +0800

    update colors

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ trans_dict_cn = dict(zip(task_list, [
 trans_dict_en = dict(zip(task_list, task_list))
 
 color_list = [
+    "#fe8c00",
     "#fc8b9d", 
     "#d0f7f6", 
     "#88b3ba", 
     "#ffcf8d", 
     "#7c67ed", 
-    "#e7e6fa",  
-    "#f08080", 
     "#fff488", 
     "#d2b48e", 
     "#207b74", 
     "#b59ba5",
     "#92d4d4", 
-    "#fe8c00",
+    "#e7e6fa",  
+    "#f08080", 
     ] # from pomotodo
