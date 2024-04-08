f7d69ac041f9673caae63427b54e87e80ef5cdf9
Junyan Su <junyansu@JunyandeMacBook-Air.local>
2024-04-08 08:00:04 +0800

    update config

--- config.py
+++ config.py
@@ -1,11 +1,25 @@
 task_list = [
-    "code","misc","meeting","writing","reading","thinking", "TA","presentation", "figure", "startup"
+    "code","misc","meeting","writing","reading","thinking", "TA","presentation", "figure", "startup", "language"
     ]
 
 trans_dict_cn = dict(zip(task_list, [
-    "代码","其它","开会","写作","阅读","思考","助教","展示","作图","创业"
+    "代码","其它","开会","写作","阅读","思考","助教","展示","作图","创业","语言"
 ]))
 
 trans_dict_en = dict(zip(task_list, task_list))
 
-color_list = ["#fc8b9d", "#d0f7f6", "#88b3ba", "#ffcf8d", "#7c67ed", "#e7e6fa", "#fe8c00", "#f08080", "#fff488", "#d2b48e", "#207b74", "#b59ba5","#92d4d4" ] # from pomotodo
+color_list = [
+    "#fc8b9d", 
+    "#d0f7f6", 
+    "#88b3ba", 
+    "#ffcf8d", 
+    "#7c67ed", 
+    "#e7e6fa",  
+    "#f08080", 
+    "#fff488", 
+    "#d2b48e", 
+    "#207b74", 
+    "#b59ba5",
+    "#92d4d4", 
+    "#fe8c00",
+    ] # from pomotodo
